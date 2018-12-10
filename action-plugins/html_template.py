eae6f5f6ffbc3a64be8a464abe54121f98e53c04
Ben Thomasson <bthomass@redhat.com>
2018-12-10 14:27:08 -0500

    Adds html_template

---
+++ action-plugins/html_template.py
@@ -0,0 +1,18 @@
+from ansible.plugins.action import ActionBase
+
+
+class ActionModule(ActionBase):
+
+    BYPASS_HOST_LOOP = True
+
+    def run(self, tmp=None, task_vars=None):
+        if task_vars is None:
+            task_vars = dict()
+        result = super(ActionModule, self).run(tmp, task_vars)
+        src = self._task.args.get('src', None)
+        with open(src) as f:
+            template_data = f.read()
+        result['text/html'] = self._templar.do_template(template_data,
+                                                        preserve_trailing_newlines=True,
+                                                        escape_backslashes=False)
+        return result
